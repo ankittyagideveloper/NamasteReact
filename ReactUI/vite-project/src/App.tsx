a98e601bdf586abffebca8e5655978f1ae5e0222
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2022-12-25 12:40:17 +0530

    assignment 1

--- ReactUI/vite-project/src/App.tsx
+++ ReactUI/vite-project/src/App.tsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+    <div>
+      <h1>Questions</h1>
+      <h2>1. Difference Between Library and Framework</h2>
+      <p>
+        “Framework is a collection of various libraries”.
+        <a
+          href="https://www.geeksforgeeks.org/software-framework-vs-library/"
+          target="_blank"
+        >
+          More info
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <h2>Important Chrome Extensions</h2>
+        <ul>
+          <li>Better Comments</li>
+          <li>Bracket Pair Colorization Toggler</li>
+          <li>ES7+ React/Redux/React-Native snippets v4</li>
+          <li>ESLint</li>
+          <li>GitLens — Git supercharged</li>
+          <li>HTML CSS Support</li>
+          <li>Live Server</li>
+          <li>Prettier ESLint</li>
+          <li>Prettier - Code formatter</li>
+          <li>Prettify JSON</li>
+          <li>Tabnine AI Autocomplete for Javascript</li>
+          <li>Tailwind CSS IntelliSense</li>
+          <li>vs code Icons</li>
+          <li>Emmet</li>
+        </ul>
+      </div>
+      <div>Learn Emmet</div>
+      <div>
+        <h3>
+          Namaste Everyone
+          <img src="./Day_1_Namaste_everyone.jpg" alt="Namaste_Everyone" />
+        </h3>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
